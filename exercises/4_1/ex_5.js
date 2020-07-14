87caee0a6f8a1710def317e8aea0c5081b9ec446
Alexandre Fogaça <alexandr.fogaca@gmail.com>
2020-07-14 18:17:00 -0300

    Exercicio 6 - Verificando uma peça de xadrez e vendo seus movimentos.

--- exercises/4_1/ex_5.js
+++ exercises/4_1/ex_5.js
@@ -3,19 +3,20 @@ let ang2 = 103;
 let ang3 = 71;
 let angInterno = ang1 + ang2 + ang3;
 
+
+if (angInterno == 180) {
+  console.log (true);
+} else {
+console.log (false);
+}
+ 
 /*
- * if (ang1 + ang2 + ang3 == 180) {
- *   console.log (true);
- * } else {
- *   console.log (false);
+ * switch (angInterno) {
+ *   case 180:
+ *    console.log (true);
+ *    break;
+ *
+ *   default:
+ *    console.log (false);
  * }
  */
-
-switch (angInterno) {
-  case 180:
-    console.log (true);
-    break;
-
-  default:
-    console.log (false);
-}
