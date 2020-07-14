58dcbbc8ca15282b1cb2880bcc2383d0c5ec5738
Alexandre Fogaça <alexandr.fogaca@gmail.com>
2020-07-14 18:21:14 -0300

    Exercicio 5 - Verificando se 3 angulos podem representar angulos internos de um triangulo

--- exercises/4_1/ex_5.js
+++ exercises/4_1/ex_5.js
@@ -3,11 +3,10 @@ let ang2 = 103;
 let ang3 = 71;
 let angInterno = ang1 + ang2 + ang3;
 
-
 if (angInterno == 180) {
   console.log (true);
 } else {
-console.log (false);
+  console.log (false);
 }
  
 /*
